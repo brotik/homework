452ddbd3c1860772e086fa8d0a1bec8ee424c119
admin <brotik13@gmail>
2019-02-09 22:56:01 +0300

    variabes added

--- lib_for_HW2.py
+++ lib_for_HW2.py
@@ -10,9 +10,11 @@ def total_rulons(room_lenght, room_width, room_hight):
     :param room_hight:
     :return:
     """
+    reserv = 0.1
+    widht_of_rulon = 1.06
     perimetr = int((room_width * 2) + (room_lenght * 2))
-    pieces_in_one_rulon = int(10 / (room_hight + 0.1))
-    all_pieces = int(perimetr / 1.06)
+    pieces_in_one_rulon = int(10 / (room_hight + reserv))
+    all_pieces = int(perimetr / widht_of_rulon)
 
     total_rulons = (math.ceil(all_pieces / pieces_in_one_rulon))
     return total_rulons
